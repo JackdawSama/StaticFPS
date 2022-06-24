ac3ba3d33e2b85a50db0e4a1ea8a79020d3bc833
Akshay <akshaybharadhwaj98@gmail.com>
2022-06-24 19:04:43 -0400

    Added groundCheck and Jump

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,8 +7,12 @@ public class PlayerMovement : MonoBehaviour
     public CharacterController controller;
     public float speed = 12f;
     public float gravity = -9.8f;
+    public float jumpHeight = 3f;
 
     public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+    bool isGrounded;
 
     float x;
     float z;
@@ -24,10 +28,22 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if(isGrounded && velocity.y > 0)
+        {
+            velocity.y = -2f;
+        }
+
         x = Input.GetAxis("Horizontal");
         z = Input.GetAxis("Vertical");
 
         move = transform.right * x + transform.forward * z;
+
+        if(Input.GetButton("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(-2f * jumpHeight * gravity);
+        }
 
         controller.Move(move * speed * Time.deltaTime);
 
