f386475b29d1392aedd336a3d2047986f382ff28
Akshay <akshaybharadhwaj98@gmail.com>
2022-07-01 01:38:02 -0400

    Modified Movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public CharacterController controller;
-    public float speed = 12f;
+    private CharacterController myCC;
+    public float playerSpeed = 12f;
     public float gravity = -9.8f;
     public float jumpHeight = 3f;
 
@@ -22,17 +22,25 @@ public class PlayerMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        myCC = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        GetInput();
+        MovePlayer();
+
+    }
+
+    void GetInput()
+    {
         isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
 
         if(isGrounded && velocity.y > 0)
         {
-            velocity.y = -2f;
+            velocity.y = 0f;
         }
 
         x = Input.GetAxis("Horizontal");
@@ -40,15 +48,19 @@ public class PlayerMovement : MonoBehaviour
 
         move = transform.right * x + transform.forward * z;
 
-        if(Input.GetButton("Jump") && isGrounded)
+        if(Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
             velocity.y = Mathf.Sqrt(-2f * jumpHeight * gravity);
         }
 
-        controller.Move(move * speed * Time.deltaTime);
+        myCC.Move(move * playerSpeed * Time.deltaTime);
 
         velocity.y += gravity * Time.deltaTime;
 
-        controller.Move(velocity * Time.deltaTime);
+    }
+
+    void MovePlayer()
+    {
+        myCC.Move(velocity * Time.deltaTime);
     }
 }
