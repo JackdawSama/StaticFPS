974ff39a27cbf3d1eb644938cb671d513673b7d9
Akshay Bharadhwaj <46255105+JackdawSama@users.noreply.github.com>
2022-10-20 00:54:42 -0400

    Created NavMesh enemies

--- Project VJRA/Assets/Scripts/EnemyController.cs
+++ Project VJRA/Assets/Scripts/EnemyController.cs
@@ -1,20 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyController : MonoBehaviour
 {
     [SerializeField] float health = 50f;
+
+    NavMeshAgent agent;
+    public GameObject player;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        agent = GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        agent.SetDestination(player.transform.position);
     }
 
     public void TakeDamage(float damage)
