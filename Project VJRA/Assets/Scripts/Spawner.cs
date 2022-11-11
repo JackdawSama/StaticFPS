60120757901f78eafeb60009e4f19ef7413fbf73
Akshay Bharadhwaj <46255105+JackdawSama@users.noreply.github.com>
2022-11-11 09:46:50 -0500

    Created Spawner and Spawner Script

---
+++ Project VJRA/Assets/Scripts/Spawner.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+
+{
+    [SerializeField] List<GameObject> KWList;
+    [SerializeField] GameObject[] enemyList;
+    [SerializeField] Transform[] enemySpawnPoints;
+    [SerializeField] GameObject enemyObject;
+    [SerializeField] float wallSpawnCD;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
